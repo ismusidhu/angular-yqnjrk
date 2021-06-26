26aa6cf39ecab9a64ab6a1ada795f5c6147aa0dc
ismusidhu <ismailsidhpuri@gmail.com>
2021-06-26 18:12:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-yqnjrk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yqnjrk)
---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-top-bar',
+  templateUrl: './top-bar.component.html',
+  styleUrls: ['./top-bar.component.css']
+})
+export class TopBarComponent implements OnInit {
+  cartItemsCount: Number = 0;
+  constructor(private cartService: CartService) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
